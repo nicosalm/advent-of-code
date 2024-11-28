8132139a6c9e0c5e3de06c6e7d220fa30724c56f
Nico Salm <nico@salm.dev>
2024-11-27 20:33:56 -0600

    setup for 2024

---
+++ 2024/5.py
@@ -0,0 +1,12 @@
+""" Advent Of Code 2024 : 5 """
+
+from aoctools import *
+
+
+def main(aocd: AOCD):
+    aocd.get_example()
+
+
+if __name__ == '__main__':
+    aocd = AOCD(2024, 5)
+    main(aocd)
