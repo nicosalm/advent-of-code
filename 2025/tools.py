858e07f0be30ddfa3cb122c5887397b0f59d52f1
Nico Salm <nico@salm.dev>
2025-12-01 01:45:04 -0600

    2025

--- 2025/tools.py
+++ 2025/tools.py
@@ -1,25 +1,18 @@
-# From: https://github.com/31b4/advent-of-code-2024/blob/main/cheatsheet.py
+# from: https://github.com/31b4/advent-of-code-2024/blob/main/cheatsheet.py
 
-# IMPORTS
-import re
 import math
-import itertools
-from collections import *
+from collections import Counter, deque, defaultdict
 import heapq
 import functools
-from copy import deepcopy
 
-# INPUTS
 raw = open("i.txt").read()
 lines = open("i.txt").read().splitlines()
 grid = [list(l.split()) for l in open("i.txt").read().splitlines()]
 
-# PARSING
 nums_lines = list(map(int, lines))
 nums_grid = [[int(x) for x in l] for l in grid]
 blocks = raw.split("\n\n")
 
-# DIRECTIONS
 DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 DIAGONALS = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
 ALL_DIRS = [(-1, 0), (-1, -1), (1, 0), (-1, 1), (0, -1), (1, -1), (0, 1), (1, 1)]
@@ -41,13 +34,11 @@ def neighbors8(grid, r, c):
         if in_bounds(grid, rr, cc):
             yield (rr, cc)
 
-# COMMON OPERATIONS
 def transpose(grid): return list(zip(*grid))
 def rotate(grid): return list(zip(*grid[::-1]))
 def flatten(lst): return [item for sublist in lst for item in sublist]
 def windows(lst, n): return list(zip(*[lst[i:] for i in range(n)]))
 
-# SEARCH/PATHS
 def manhattan(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
@@ -79,14 +70,13 @@ def dijkstra(start):
                 heapq.heappush(pq, (new_dist, npos))
     return dist
 
-# MATH
 MOD = 10**9 + 7
 INF = float('inf')
+
 def lcm(a, b): return abs(a * b) // math.gcd(a, b)
 def lcm_list(lst): return functools.reduce(lcm, lst)
 def prod(lst): return functools.reduce(lambda x,y: x*y, lst)
 
-# USEFUL SETS/RANGES
 def range_intersection(r1, r2):
     return range(max(r1.start, r2.start), min(r1.stop, r2.stop))
 
@@ -95,7 +85,6 @@ def range_union(r1, r2):
         return None
     return range(min(r1.start, r2.start), max(r1.stop, r2.stop))
 
-# BINARY SEARCH
 def binary_search(arr, x):
     l, r = 0, len(arr)
     while l < r:
@@ -106,7 +95,6 @@ def binary_search(arr, x):
             r = m
     return l
 
-# COUNTER TRICKS
 def most_common(lst):
     return Counter(lst).most_common(1)[0][0]
 
